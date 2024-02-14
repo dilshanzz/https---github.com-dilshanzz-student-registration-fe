e39a05a8b2df86b94b1bade263d083e0a8dd01e4
dilshanzz <dilshanwijewardhana999@gmail.com>
2024-02-14 07:28:36 +0530

    connect front end and backend addstudent complete

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
-document.getElementById("btn-proceed").addEventListener("click", proceed)
+document.getElementById("btn-proceed").
+        addEventListener("click", proceed)
 
 function proceed() {
     let firstName = document.getElementById("first-name").value;
     let lastName = document.getElementById("last-name").value;
+    let contact = document.getElementById("contact").value
 
-    console.log(firstName);
-    console.log(lastName);
+    let studentReq= {
+        "firstName" : firstName,
+         "lastName" : lastName,
+        "contact" : contact
+    }
+
+    fetch("http://localhost:8080/student",{
+        method: "POST",
+        body: JSON.stringify(studentReq),
+       headers:{
+        "Content-Type":"application/json"
+       }
+    })
+    .then((response) => response.JSON())
+    .then((data) =>{
+
+    })
+     console.log(studentReq);
+    // console.log(lastName);
 }
